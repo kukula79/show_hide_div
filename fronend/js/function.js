1b23b079091e6221aa76506f273cd9e7bdddef95
Yosi Habushi <yhabushi@allot.com>
2020-04-06 11:54:53 +0300

    - improve js function

--- fronend/js/function.js
+++ fronend/js/function.js
@@ -1,23 +1,13 @@
 $(document).ready(function(){
  $("#content").hide();
-
  $('#nms_product').on('change', function() {
-    if ($('#nms_product').val() == "none"){
-            show_hide($('#nms_product').val(),$('#nms_mode').val());
-    } else {
-        show_hide($('#nms_product').val(),$('#nms_mode').val());
-        }
+    show_hide($('#nms_product').val(),$('#nms_mode').val());
      });
  $('#nms_mode').on('change', function() {
-    if ($('#nms_mode').val() == "none"){
-            show_hide($('#nms_product').val(),$('#nms_mode').val());
-    } else {
-        show_hide($('#nms_product').val(),$('#nms_mode').val());
-        }
+    show_hide($('#nms_product').val(),$('#nms_mode').val());
      });
 
 function show_hide(product,mode){
-
     if ( product != "none"  &&  mode != "none" ){
         $("#content").show();
         console.log("User select nms_product : "+ product+ " And nms_mode :"+mode + "-> Display");
